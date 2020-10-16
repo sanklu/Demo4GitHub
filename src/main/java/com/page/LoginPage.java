5507e2591216e85ba5fbcafed4a3cb6218670d37
Sankalp <snklp7108@gmail.com>
2020-10-16 21:26:33 +0530

    Second commit

--- src/main/java/com/page/LoginPage.java
+++ src/main/java/com/page/LoginPage.java
@@ -3,7 +3,8 @@ package com.page;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is login Page");
+		System.out.println("New comment is added");
 
 	}
 
